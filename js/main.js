17004d9dbc5318bfcbf75078d2462704ca72f8a7
Lukasz Kornek <lk0001@wp.pl>
2013-12-21 12:46:06 +0100

    params, params, params

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
   function diffUsingJS(opts={}) {
     "use strict";
-    var viewType = opts["viewType"],
+    var viewType = opts["viewType"] || 0,
       baseId = opts["baseId"],
       newId = opts["newId"],
-      outputDiv = opts["outputDiv"];
+      contextSizeInputId = opts["contextSizeInputId"],
+      outputDiv = opts["outputDiv"],
+      baseTextName = opts["baseTextName"] || "Base Text";
+      newTextName = opts["newTextName"] || "New Text";
 
     var base = difflib.stringAsLines($(baseId).val()),
       newtxt = difflib.stringAsLines($(newId).val()),
       sm = new difflib.SequenceMatcher(base, newtxt),
       opcodes = sm.get_opcodes(),
       diffoutputdiv = $(outputDiv)[0],
-      contextSize = $("#contextSize").val();
+      contextSize = $(contextSizeInputId).val();
 
     diffoutputdiv.innerHTML = "";
     contextSize = contextSize || null;
@@ -20,17 +23,17 @@ $(document).ready(function () {
       baseTextLines: base,
       newTextLines: newtxt,
       opcodes: opcodes,
-      baseTextName: "Base Text",
-      newTextName: "New Text",
+      baseTextName: baseTextName,
+      newTextName: newTextName,
       contextSize: contextSize,
       viewType: viewType
     }));
   };
 
   function displayDiff() {
-    diffUsingJS({"viewType": 0,
-                 "baseId": "#baseText",
+    diffUsingJS({"baseId": "#baseText",
                  "newId": "#newText",
+                 "contextSizeInputId": "#contextSize"
                  "outputDiv": "#diffoutput"
     });
   };
@@ -38,4 +41,3 @@ $(document).ready(function () {
   $("#baseText, #newText").bind("input propertychange", displayDiff);
   displayDiff();
 });
-

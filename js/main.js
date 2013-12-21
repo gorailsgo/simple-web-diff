c2dafd15648467fcc299d968159ab4434171adf8
Piotrek Okoński <piotrek@okonski.org>
2013-12-21 12:43:34 +0000

    Add basic design and File API.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
+$(document).foundation();
 
+var onDragEnter = function(event) {
+    event.preventDefault();
+    $(this).addClass("dragover");
+},
+
+onDragOver = function(event) {
+    event.preventDefault();
+    if(!$(this).hasClass("dragover"))
+        $(this).addClass("dragover");
+},
+
+onDragLeave = function(event) {
+    event.preventDefault();
+    $(this).removeClass("dragover");
+},
+
+onDrop = function(event) {
+    var droppable = $(this);
+    $('#text-error-alert').hide();
+    event.preventDefault();
+    $(this).removeClass("dragover");
+    var file = event.originalEvent.dataTransfer.files[0];
+    console.log(file);
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var result = reader.result;
+
+      droppable.data('content', reader.result);
+      console.log(result.length);
+    }
+    reader.readAsText(file);
+    $(this).find('.filename').text(file.name);
+
+}
+
+$(document).ready(function () {
+  var dropBase = $('#drop-base'),
+      dropChanged = $('#drop-changed');
+  $('.droparea').on("dragenter", onDragEnter)
+    .on("dragover", onDragOver)
+    .on("dragleave", onDragLeave)
+    .on("drop", onDrop);
+});
